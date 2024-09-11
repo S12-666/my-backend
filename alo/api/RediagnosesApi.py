f9fe16d8a36e21f71d930b02f99b6f95ef189dcd
WenJu Liu <lius90613@gmail.com>
2024-09-11 15:12:52 +0800

    修改8804再诊断接口

--- alo/api/RediagnosesApi.py
+++ alo/api/RediagnosesApi.py
@@ -6,12 +6,17 @@ from ..controller.Rediagnoses import RediagnosesController
 parser = reqparse.RequestParser(trim=True, bundle_errors=True)
 from . import api
 class RediagnosesApi(Resource):
-    def post(self):
+    def post(self, fault_type):
         args = getArgsFromParser(parser, ['condition', 'testData', 'selectedKey'])
-        condition = json.loads(args['condition'])
-        testData = json.loads(args['testData'])
-        selectedKey = json.loads(args['selectedKey'])
-        rediag = RediagnosesController(condition, testData, selectedKey)
+
+        para = {}
+        for key in args:
+            para[key] = json.loads(args[key])
+        para['fault_type'] = fault_type
+        # condition = json.loads(args['condition'])
+        # testData = json.loads(args['testData'])
+        # selectedKey = json.loads(args['selectedKey'])
+        rediag = RediagnosesController(para)
         res = rediag.run()
         response = {
             'code': 0,
@@ -20,4 +25,4 @@ class RediagnosesApi(Resource):
         }
         return response, 200, {'Access-Control-Allow-Origin': '*'}
 
-api.add_resource(RediagnosesApi, '/v1.0/visualization/rediagnoses')
+api.add_resource(RediagnosesApi, '/v1.0/visualization/rediagnoses/<fault_type>')
--- alo/controller/Rediagnoses.py
+++ alo/controller/Rediagnoses.py
@@ -5,25 +5,32 @@ from ..methods.dataProcessing import rawDataToModelData, plateHasDefect, plateDe
 from ..methods.define import data_names, flag_names, name_index
 from ..methods.DiagnosesAlgorithm import DiagnosesAlgorithm
 class RediagnosesController:
-    def __init__(self, args, test, selectedKey):
-        self.args = args
-        self.selectedKey = selectedKey
+    def __init__(self, args):
+        self.args = {
+            'test': args['testData'],
+            'selectedKey': args['selectedKey'],
+            'condition': args['condition'],
+            'fault_type': args['fault_type'],
+        }
+        # self.args = args
+        # self.selectedKey = selectedKey
+        # self.fault_type = fault_type
         test_list = []
         for name in data_names:
             try:
-                val = test[name]
+                val = self.args['test'][name]
                 test_list.append(val)
             except:
                 test_list.append(0)
-        self.test = [test_list]
+        self.args['test'] = [test_list]
     def run(self):
         train_raw, train_col = diagnosesTrainDataByArgs(self.args)
         train_df = pd.DataFrame(data=train_raw, columns=train_col)
-        test_matrix = self.test
+        test_matrix = self.args['test']
         train_matrix = []
-        train_df['label'] = train_df[['status_fqc', 'fqc_label']].apply(lambda x: plateHasDefect(x['status_fqc'], x['fqc_label']), axis=1)
+        train_df['label'] = train_df[['status_fqc', 'p_f_label']].apply(lambda x: plateHasDefect(x['status_fqc'], x['p_f_label']), axis=1)
         for idx, flag in enumerate(flag_names):
-            train_df[flag] = train_df[['status_fqc', 'fqc_label']].apply(lambda x: plateDetailedDefect(x['status_fqc'], x['fqc_label'], idx), axis=1)
+            train_df[flag] = train_df[['status_fqc', 'p_f_label']].apply(lambda x: plateDetailedDefect(x['status_fqc'], x['p_f_label'], idx), axis=1)
             good_train_df = train_df[train_df[flag] == 1]
             data_matrix, _ = rawDataToModelData(good_train_df)
             train_matrix.append(data_matrix)
